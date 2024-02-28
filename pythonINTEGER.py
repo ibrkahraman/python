ea0ecddc57acbb8c255b062330f8788b7a7c2e7a
ibrkahraman <161464501+ibrkahraman@users.noreply.github.com>
2024-02-28 18:42:12 +0300

    Add files via upload

---
+++ pythonINTEGER.py
@@ -0,0 +1,26 @@
+#Mutlak deger" abs() "
+
+
+#yuvarlama " round() ":
+sayi = 3.1475869
+print(round(sayi,3))
+print(round(sayi,2))
+print(round(sayi))
+
+
+#kuvvet alma " ** "
+
+
+#Tamsayi sonuclu bolme " // "
+
+
+#Esit degildir " != "
+
+
+#Değer verilmiş değişkenin, değerini değiştirmek:
+i = 7
+i +=5
+print(i)
+k = 5
+k *= 6
+print(k)
