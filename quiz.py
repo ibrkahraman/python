a9ecf2c630807056cf1c72df123b106621c0977a
ibrkahraman <161464501+ibrkahraman@users.noreply.github.com>
2024-03-05 14:16:39 +0300

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,30 @@
+cevap5 = "15"
+cevap4 = "türkçe"
+cevap3 = "ibrahim"
+cevap2 = "10"
+cevap1 = "iyi"
+cevap6 = input("nasılsın?")
+cevap7 = input(" bir günde kaç ders var")
+cevap8 = input("adım ne?")
+cevap9 = input("yazdığım dil ne")
+cevap10 = input("yaşım kaç")
+if cevap1 == cevap6:
+    print("+5")
+else:
+    print("-5")
+if cevap2 == cevap7:
+    print("+5")
+else:
+    print("-5")
+if cevap3 == cevap8:
+    print("+5")
+else:
+    print("-5")
+if cevap4 == cevap9:
+    print("+5")
+else:
+    print("-5")
+if cevap5 == cevap10:
+    print("+5")
+else:
+    print("-5")
